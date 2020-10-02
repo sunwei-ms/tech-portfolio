0f5899af2a99664012f97a22573d9b6c10156e79
Wei Sun <suwe@microsoft.com>
2020-10-02 12:34:05 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -13,7 +13,7 @@ const users = [];
 
 const siteConfig = {
     title: 'Wei Sun', // Title for your website.
-    tagline: 'tagline',
+    tagline: 'We build the future',
     profilePhoto: 'img/profile.png',
     url: 'https://sunwei-ms.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
